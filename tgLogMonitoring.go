fc2f4886f60216af36996f18b381718f3fa3f333
AmadoMuerte <ksik854@gmail.com>
2024-07-28 16:26:46 +1000

    refactor

--- tgLogMonitoring.go
+++ tgLogMonitoring.go
@@ -4,28 +4,32 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"time"
 )
 
 type Config struct {
-	BotToken string
-	ChatsId  []string
+	botToken string
+	chatsId  []string
 }
 
-func NewLog(cfg Config, message string) {
-	tgUrl := "https://api.telegram.org/bot" + cfg.BotToken + "/sendMessage"
+type LogMonitoring struct {
+	cfg Config
+}
 
-	for i := 0; i < len(cfg.ChatsId); i++ {
-		req, _ := http.NewRequest("POST", tgUrl, nil)
-		q := req.URL.Query()
-		q.Add("chat_id", cfg.ChatsId[i])
-		q.Add("text", message)
-		req.URL.RawQuery = q.Encode()
+func (l *LogMonitoring) NewLog(message string) {
+	tgUrl := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", l.cfg.botToken)
 
-		client := &http.Client{}
-		resp, err := client.Do(req)
+	for _, chatID := range l.cfg.chatsId {
+		message = time.Now().Format("2006-01-02 15:04:05") + "\n" + url.QueryEscape(message)
+		messageLength := len(message)
+		if messageLength > 4096 {
+			message = message[:4096]
+		}
+		resp, err := http.PostForm(tgUrl, url.Values{"chat_id": {chatID}, "text": {}})
 		if err != nil {
 			fmt.Println("Error sending request:", err)
-			return
+			continue
 		}
 		defer func(Body io.ReadCloser) {
 			err := Body.Close()
