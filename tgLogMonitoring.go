19587e99f96f35f5c1f415a613ebc19a68e3886a
AmadoMuerte <ksik854@gmail.com>
2024-07-28 17:19:33 +1000

    refactor

--- tgLogMonitoring.go
+++ tgLogMonitoring.go
@@ -9,18 +9,18 @@ import (
 )
 
 type Config struct {
-	botToken string
-	chatsId  []string
+	BotToken string
+	ChatsId  []string
 }
 
 type New struct {
-	cfg Config
+	Cfg Config
 }
 
 func (l *New) Send(message string) {
-	tgUrl := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", l.cfg.botToken)
+	tgUrl := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", l.Cfg.BotToken)
 
-	for _, chatID := range l.cfg.chatsId {
+	for _, chatID := range l.Cfg.ChatsId {
 		message = time.Now().Format("2006-01-02 15:04:05") + "\n" + url.QueryEscape(message)
 		messageLength := len(message)
 		if messageLength > 4096 {
