94416c51f7a6d071c7a9fab5aaac960ce195ffd4
AmadoMuerte <kisik854@gmail.com>
2024-10-14 16:49:45 +1000

    added dateShow cfg and rewrite requests on goroutines

--- tgLogMonitoring.go
+++ tgLogMonitoring.go
@@ -2,43 +2,60 @@ package tgLogMonitoring
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
+	"sync"
 	"time"
 )
 
 type Config struct {
+	// telegram bot token
 	BotToken string
-	ChatsId  []string
+	// telegram chats id
+	ChatsId []string
+	// show date now
+	DateShow bool
 }
 
 type New struct {
 	Cfg Config
 }
 
-func (l *New) Send(message string, title string) {
+func (l *New) Send(message interface{}, title string) {
 	tgUrl := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", l.Cfg.BotToken)
 
-	currentTime := time.Now().Format("2006-01-02 15:04:05")
-	formattedMessage := fmt.Sprintf("<strong>%s - %s</strong>\n<code>%s</code>", title, currentTime, message)
+	dateNow := time.Now().Format("2006-01-02 15:04:05")
+	messageStr := fmt.Sprintf("%v", message)
+	var formattedMessage string
+
+	switch l.Cfg.DateShow {
+	case true:
+		formattedMessage = fmt.Sprintf("<b>%s - %s</b>\n<pre language='bash'>%s</pre>", title, dateNow, messageStr)
+
+	case false:
+		formattedMessage = fmt.Sprintf("<b>%s</b>\n<pre language='bash'>%s</pre>", title, messageStr)
+	}
+
+	var wg sync.WaitGroup
 
 	for _, chatID := range l.Cfg.ChatsId {
-		values := url.Values{
-			"chat_id":    {chatID},
-			"text":       {formattedMessage},
-			"parse_mode": {"html"},
-		}
-		resp, err := http.PostForm(tgUrl, values)
-		if err != nil {
-			fmt.Println("Error sending request:", err)
-			continue
-		}
-		defer func(body io.ReadCloser) {
-			err := body.Close()
-			if err != nil {
-				fmt.Println("Error closing response body:", err)
+		wg.Add(1)
+		go func(chatID string) {
+			defer wg.Done()
+
+			values := url.Values{
+				"chat_id":    {chatID},
+				"text":       {formattedMessage},
+				"parse_mode": {"html"},
 			}
-		}(resp.Body)
+			resp, err := http.PostForm(tgUrl, values)
+			if err != nil {
+				fmt.Println("Error sending request:", err)
+				return
+			}
+			defer resp.Body.Close()
+		}(chatID)
 	}
+
+	wg.Wait()
 }
